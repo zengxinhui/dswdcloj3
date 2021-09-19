f83274807f9796a10b8bb6e8fe04d38c9c27f29d
Xinhui Zeng <zengxh@gmail.com>
2021-09-19 10:35:05 -0400

    coercion and validation with reitit

--- src/clj/guestbook/routes/services.clj
+++ src/clj/guestbook/routes/services.clj
@@ -2,13 +2,37 @@
   (:require
    [reitit.swagger :as swagger]
    [reitit.swagger-ui :as swagger-ui]
+   [reitit.ring.coercion :as coercion]
+   [reitit.coercion.spec :as spec-coercion]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.ring.middleware.exception :as exception]
+   [reitit.ring.middleware.multipart :as multipart]
+   [reitit.ring.middleware.parameters :as parameters]
    [guestbook.messages :as msg]
    [guestbook.middleware :as middleware]
+   [guestbook.middleware.formats :as formats]
    [ring.util.http-response :as response]))
 
 (defn service-routes []
   ["/api"
-   {:middleware [middleware/wrap-formats]
+   {:middleware [;; query-params & form-params
+                 parameters/parameters-middleware
+                 ;; content-negotiation
+                 muuntaja/format-negotiate-middleware
+                 ;; encoding response body
+                 muuntaja/format-response-middleware
+                 ;; exception handling
+                 exception/exception-middleware
+                 ;; decoding request body
+                 muuntaja/format-request-middleware
+                 ;; coercing response bodys
+                 coercion/coerce-response-middleware
+                 ;; coercing request parameters
+                 coercion/coerce-request-middleware
+                 ;; multipart params
+                 multipart/multipart-middleware]
+    :muuntaja formats/instance
+    :coercion spec-coercion/coercion
     :swagger {:id ::api}}
    ["" {:no-doc true}
     ["/swagger.json"
@@ -18,21 +42,42 @@
             {:url "/api/swagger.json"})}]]
    ["/messages"
     {:get
-     (fn [_]
-       (response/ok (msg/message-list)))}]
+     {:responses
+      {200
+       {:body ;; Data Spec for response body
+        {:messages
+         [{:id pos-int?
+           :name string?
+           :message string?
+           :timestamp inst?}]}}}
+      :handler
+      (fn [_]
+        (response/ok (msg/message-list)))}}]
    ["/message"
     {:post
-     (fn [{:keys [params]}]
-       (try
-         (msg/save-message! params)
-         (response/ok {:status :ok})
-         (catch Exception e
-           (let [{id :guestbook/error-id
-                  errors :errors} (ex-data e)]
-             (case id
-               :validation
-               (response/bad-request {:errors errors})
-               ;;else
-               (response/internal-server-error
-                {:errors
-                 {:server-error ["Failed to save message!"]}}))))))}]])
+     {:parameters
+      {:body ;; Data Spec for Request body parameters
+       {:name string?
+        :message string?}}
+      :responses
+      {200
+       {:body map?}
+       400
+       {:body map?}
+       500
+       {:errors map?}}
+      :handler
+      (fn [{{params :body} :parameters}]
+        (try
+          (msg/save-message! params)
+          (response/ok {:status :ok})
+          (catch Exception e
+            (let [{id :guestbook/error-id
+                   errors :errors} (ex-data e)]
+              (case id
+                :validation
+                (response/bad-request {:errors errors})
+                ;;else
+                (response/internal-server-error
+                 {:errors
+                  {:server-error ["Failed to save message!"]}}))))))}}]])
