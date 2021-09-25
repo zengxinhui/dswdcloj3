0f94790ec3f36f9a6105a92cb9eac894837dcad1
Xinhui Zeng <zengxh@gmail.com>
2021-09-25 00:49:31 -0400

    The Elephant in the Room

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,9 +1,9 @@
 -- :name save-message! :! :n
 -- :doc creates a new message using the name and message keys
-INSERT INTO guestbook
+INSERT INTO posts
 (name, message)
 VALUES (:name, :message)
 
 -- :name get-messages :? :*
 -- :doc selects all available messages
-SELECT * from guestbook
+SELECT * from posts
