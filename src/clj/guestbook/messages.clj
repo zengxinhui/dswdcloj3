a00ce3b4471e72581a840a4cc4127630e8a7f27b
Xinhui Zeng <zengxh@gmail.com>
2021-10-04 22:29:51 -0400

    Add missed update to save-message! in messages.clj

--- src/clj/guestbook/messages.clj
+++ src/clj/guestbook/messages.clj
@@ -6,12 +6,16 @@
 (defn message-list []
   {:messages (vec (db/get-messages))})
 
-(defn save-message! [{:keys [login]} message]
+(defn save-message! [{{:keys [display-name]} :profile
+                      :keys [login]}
+                     message]
   (if-let [errors (validate-message message)]
     (throw (ex-info "Message is invalid"
                     {:guestbook/error-id :validation
                      :errors errors}))
-    (db/save-message! (assoc message :author login))))
+    (db/save-message! (assoc message
+                             :author login
+                             :name (or display-name login)))))
 
 (defn messages-by-author [author]
   {:messages (vec (db/get-messages-by-author {:author author}))})
